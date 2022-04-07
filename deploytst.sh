4487b8eea06163ffcf3784401430e9480dd5f44b
frmauro <frmauro8@gmail.com>
2022-04-07 15:48:06 -0300

    many changes 003

--- deploytst.sh
+++ deploytst.sh
@@ -6,28 +6,28 @@
 ########################################################
 
 
-# result=$( sudo docker images -q apitest01 )
-# if [[ -n "$result" ]]; then
-# echo "image exists"
-# sudo docker rmi -f apitest01
-# else
-# echo "No such image"
-# fi
+result=$( docker images -q javaapitest01 )
+if [[ -n "$result" ]]; then
+  echo "image exists"
+  docker rmi -f javaapitest01
+else
+  echo "No such image"
+fi
 
-# echo "build the docker image"
 
-# echo "built docker images and proceeding to delete existing container"
-# result=$( docker ps -q -f name=apitest01 )
-# if [[ $? -eq 0 ]]; then
-# echo "Container exists"
-# sudo docker container rm -f apitest01
-# echo "Deleted the existing docker container"
-# else
-# echo "No such container"
-# fi
+echo "built docker images and proceeding to delete existing container"
+result=$( docker ps -q -f name=javaapitest01 )
+if [[ $? -eq 0 ]]; then
+  echo "Container exists"
+   docker container rm -f javaapitest01
+  echo "Deleted the existing docker container"
+else
+  echo "No such container"
+fi
 
 cp -a /home/francisco/estudos/azuredevops/myagent/_work/4/s/.  /home/francisco/estudos/azuredevops/myagent/_work/r3/a/
 
+echo "build the docker image"
 docker build -t javaapitest01 .
 
 echo "built docker images and proceeding to delete existing container"
